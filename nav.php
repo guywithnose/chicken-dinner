0afa528ff3277fbed559a9f1c1299064647b4aac
Chris Ryan <christopher.ryan@dominionenterprises.com>
2013-10-04 12:50:47 -0400

    Added facebook like button

--- nav.php
+++ nav.php
@@ -1,3 +1,11 @@
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=535495296519799";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
     <div class="navbar navbar-inverse navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
@@ -13,5 +21,8 @@
             <li><a href="about.php">About</a></li>
           </ul>
         </div><!--/.navbar-collapse -->
+        <div class="social">
+            <div class="fb-like" data-href="http://chicken-dinner.herokuapp.com/" data-width="200" data-height="50" data-colorscheme="dark" data-layout="button_count" data-action="like" data-show-faces="true" data-send="false"></div>
+        </div>
       </div>
     </div>
