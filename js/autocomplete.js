b61b6996ecf127cefb838980c45630130f04858c
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2013-10-04 12:58:46 -0400

    Add rental fees.

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -21,9 +21,14 @@ $(function() {
             };
 
             var incomeRanges = ["0-10k","10-15k","15-20k","20-25k","25-30k","30-35k","35-40k","40-45k","45-50k","50-60k","60-75k","75-100k","100-125k","125-150k","150-200k"];
+            var rentRanges = ["0-100","100-150","150-200","200-250","250-300","300-350","350-400","400-450","450-500","500-550","550-600","600-650","650-700","700-750","750-800","800-900","900-1000","1000-1250","1250-1500","1500-2000","2000+"];
 
             for (var i in incomeRanges) {
                 availableTags.push('Households that make $' + incomeRanges[i]);
+            }
+
+            for (var i in rentRanges) {
+                availableTags.push('Rental fees of $' + rentRanges[i]);
             }
 
             availableTags.push('Population Density');
@@ -74,6 +79,14 @@ $(function() {
                             vehicleCounts = d3.map();
                             queue().defer(d3.json, "data/us.json")
                                 .defer(d3.csv, "data/income-fips.csv", function(d) { vehicleCounts.set(d.fips, +d[incomeRange]); })
+                                .await(ready);
+                        } else if (value.search('Rental fees of \\$') != -1) {
+                            var rentalRange = value.replace('Rental fees of $', '');
+                            legendTitle = '# of households with rental fees of $' + rentalRange;
+                            $('#huge-map').html('<div class="loading">&nbsp;</div>');
+                            vehicleCounts = d3.map();
+                            queue().defer(d3.json, "data/us.json")
+                                .defer(d3.csv, "data/rent-fips.csv", function(d) { vehicleCounts.set(d.fips, +d[rentalRange]); })
                                 .await(ready);
                         }
                     }
